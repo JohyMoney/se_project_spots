b793f99d7eb9ba9a0d69fbb3eb197d576d6ea785
JohnnyMoney <wyndrockjohn@gmail.com>
2026-01-06 16:04:17 -0500

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editprofileclosebtn = editProfileModal.querySelector(".modal__close-btn");
+
+const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+console.log("editProfileBtn:", editProfileBtn);
+console.log("editProfileModal:", editProfileModal);
+console.log("newPostBtn:", newPostBtn);
+console.log("newPostModal:", newPostModal);
+
+editProfileBtn.addEventListener("click", function () {editProfileModal.classList.add("modal__container--opened");}
+);
+
+editprofileclosebtn.addEventListener("click", function () {editProfileModal.classList.remove("modal__container--opened");}
+);
+
+newPostBtn.addEventListener("click", function () {newPostModal.classList.add("modal__is-opened");}
+);
+
+newPostCloseBtn.addEventListener("click", function () {newPostModal.classList.remove("modal__is-opened");});
